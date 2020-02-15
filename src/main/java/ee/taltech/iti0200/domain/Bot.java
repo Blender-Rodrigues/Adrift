fc6f3660dcdb88334a25031fbed3edb1d8a5618a
Kristjan Kõiv <krikoi@ttu.ee>
2020-02-15 20:58:39 +0200

    #7 Defined a Bot that extends Entity.

---
+++ src/main/java/ee/taltech/iti0200/domain/Bot.java
@@ -0,0 +1,16 @@
+package ee.taltech.iti0200.domain;
+
+import ee.taltech.iti0200.physics.Body;
+
+import javax.vecmath.Vector2d;
+import java.util.Arrays;
+
+public class Bot extends Entity {
+
+    private static final Vector2d size = new Vector2d(1.5, 1.5);
+    private static final double mass = 70.0;
+
+    public Bot(Vector2d position) {
+        super(Arrays.asList(new Body(mass, size, position, true)));
+    }
+}
