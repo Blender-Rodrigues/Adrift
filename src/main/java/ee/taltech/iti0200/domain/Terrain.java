88aec6422dc1d1c6f07a87b0f5ba5ab4d9e0870a
Kristjan Kõiv <krikoi@ttu.ee>
2020-03-04 13:09:36 +0200

    #24 Made sure that Terrain position vector is rounded to 2 decimal places.

--- src/main/java/ee/taltech/iti0200/domain/Terrain.java
+++ src/main/java/ee/taltech/iti0200/domain/Terrain.java
@@ -5,12 +5,12 @@ import ee.taltech.iti0200.physics.Vector;
 
 public class Terrain extends Entity {
 
-    private static final Vector size = new Vector(2.0, 2.0);
+    private static final Vector size = new Vector(2.00, 2.00);
     private static final double mass = Double.POSITIVE_INFINITY;
     private static final double elasticity = 0.9;
 
     public Terrain(Vector position) {
-        super(new Body(mass, new Vector(size), position, true, true), true);
+        super(new Body(mass, new Vector(size), Vector.roundVector(position), true, true), true);
         setElasticity(elasticity);
     }
 
--- src/main/java/ee/taltech/iti0200/physics/Vector.java
+++ src/main/java/ee/taltech/iti0200/physics/Vector.java
@@ -26,4 +26,12 @@ public class Vector extends Vector2d {
         this.setX(this.getX() * vector.getX());
         this.setY(this.getY() * vector.getY());
     }
+
+    public static Vector roundVector(Vector2d vector) {
+        int xInt = (int) vector.getX() * 100;
+        int yInt = (int) vector.getY() * 100;
+        double x = (double) xInt / 100d;
+        double y = (double) yInt / 100d;
+        return new Vector(x, y);
+    }
 }
