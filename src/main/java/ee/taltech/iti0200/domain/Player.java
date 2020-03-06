2d1e0e88c3fad9b1ca49bddf7ffb2d0e14990b49
Kristjan Kõiv <krikoi@ttu.ee>
2020-03-06 09:40:44 +0200

    #16 Added counters to Player that track how many times the player is allowed to jump in a row and how many times he has jumped in a row.

--- src/main/java/ee/taltech/iti0200/domain/Player.java
+++ src/main/java/ee/taltech/iti0200/domain/Player.java
@@ -8,9 +8,29 @@ public class Player extends Entity {
     private static final Vector size = new Vector(1.5, 1.5);
     private static final double mass = 70.0;
     private static final double elasticity = 0.25;
+    private int jumpAmountLimit;
+    private int jumpedSoFar;
 
     public Player(Vector position) {
         super(new Body(mass, new Vector(size), position, true, true), false);
         setElasticity(elasticity);
+        setJumpAmountLimit(2);
+        setJumpedSoFar(0);
+    }
+
+    public void setJumpAmountLimit(int jumpAmountLimit) {
+        this.jumpAmountLimit = jumpAmountLimit;
+    }
+
+    public int getJumpAmountLimit() {
+        return this.jumpAmountLimit;
+    }
+
+    public void setJumpedSoFar(int jumpedSoFar) {
+        this.jumpedSoFar = jumpedSoFar;
+    }
+
+    public int getJumpedSoFar() {
+        return this.jumpedSoFar;
     }
 }
