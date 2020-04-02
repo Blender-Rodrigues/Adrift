dd0999cc2bd4d6b6dc2c1fedd8eee9e079d543fd
Kristjan Kõiv <krikoi@ttu.ee>
2020-04-02 21:21:34 +0300

    #51 Added more tests.

--- src/test/java/ee/taltech/iti0200/ai/IntelligenceTest.java
+++ src/test/java/ee/taltech/iti0200/ai/IntelligenceTest.java
@@ -3,13 +3,19 @@ package ee.taltech.iti0200.ai;
 import ee.taltech.iti0200.di.factory.BotFactory;
 import ee.taltech.iti0200.domain.World;
 import ee.taltech.iti0200.domain.entity.Bot;
+import ee.taltech.iti0200.domain.entity.Player;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -33,6 +39,52 @@ class IntelligenceTest {
         verify(world).addEntity(captor.capture());
         Bot addedBot = captor.getValue();
         assertThat(addedBot.getId()).isEqualTo(id);
+    }
+
+    @Test
+    void spawnBotAfterAddingPlayer() {
+        ArgumentCaptor<Bot> captor = ArgumentCaptor.forClass(Bot.class);
+        UUID id = UUID.randomUUID();
+
+        Bot bot = mock(Bot.class);
+        World world = mock(World.class);
+        BotFactory botFactory = mock(BotFactory.class);
+        Intelligence intelligence = new Intelligence(world, botFactory);
+
+        when(bot.getId()).thenReturn(id);
+        when(botFactory.create()).thenReturn(bot);
+
+        intelligence.update(0);
+        world.addEntity(mock(Player.class));
+        intelligence.update(10);
+
+        verify(world, times(3)).addEntity(captor.capture());
+        List<Bot> addedBots = captor.getAllValues();
+        assertThat(addedBots.size()).isEqualTo(3);
+        assertThat(addedBots.get(0).getId()).isEqualTo(id);
+    }
+
+    @Test
+    void spawnBotAfterKillingBot() {
+        ArgumentCaptor<Bot> captor = ArgumentCaptor.forClass(Bot.class);
+        UUID id = UUID.randomUUID();
+
+        Bot bot = mock(Bot.class);
+        World world = mock(World.class, RETURNS_DEEP_STUBS);
+        BotFactory botFactory = mock(BotFactory.class);
+        Intelligence intelligence = new Intelligence(world, botFactory);
+
+        when(bot.getId()).thenReturn(id);
+        when(botFactory.create()).thenReturn(bot);
+
+        intelligence.update(0);
+        bot.setAlive(false);
+        intelligence.update(10);
+
+        verify(world, times(2)).addEntity(captor.capture());
+        List<Bot> addedBots = captor.getAllValues();
+        assertThat(addedBots.size()).isEqualTo(2);
+        assertThat(addedBots.get(1).getId()).isEqualTo(id);
     }
 
 }
