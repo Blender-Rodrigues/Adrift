66bfe3727ed8d68eada2a716819512f044c6c426
Jaak Kütt <jaak.kytt@gmail.com>
2020-04-02 17:18:44 +0300

    Make custom gui test annotation

--- src/test/java/ee/taltech/iti0200/graphics/GraphicsTest.java
+++ src/test/java/ee/taltech/iti0200/graphics/GraphicsTest.java
@@ -8,7 +8,15 @@ import ee.taltech.iti0200.di.GuiModule;
 import ee.taltech.iti0200.di.annotations.WindowId;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.lwjgl.opengl.GL;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import static java.util.Arrays.asList;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
@@ -20,8 +28,7 @@ import static org.lwjgl.glfw.GLFW.glfwTerminate;
  * Use this parent class to provide gl context if you're not testing graphics components.
  * Don't use it together with Graphics class itself as it creates its own context.
  *
- * P.S. disable gui tests using the following annotation on the class or test method level
- * @DisabledIfEnvironmentVariable(named = "NO_GUI", matches = "true")
+ * P.S. Annotate gui tests with @GuiTest instead of @Test to ignore them in build server which doesn't have gui.
  */
 abstract public class GraphicsTest {
 
@@ -41,6 +48,15 @@ abstract public class GraphicsTest {
         glfwFreeCallbacks(window);
         glfwDestroyWindow(window);
         glfwTerminate();
+    }
+
+    @Target({ ElementType.ANNOTATION_TYPE, ElementType.TYPE, ElementType.METHOD })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Test
+    @Inherited
+    @DisabledIfEnvironmentVariable(named = "NO_GUI", matches = "true")
+    public @interface GuiTest {
+
     }
 
 }
--- src/test/java/ee/taltech/iti0200/graphics/ShaderTest.java
+++ src/test/java/ee/taltech/iti0200/graphics/ShaderTest.java
@@ -1,14 +1,10 @@
 package ee.taltech.iti0200.graphics;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
-
 import java.io.IOException;
 
-@DisabledIfEnvironmentVariable(named = "NO_GUI", matches = "true")
 class ShaderTest extends GraphicsTest {
 
-    @Test
+    @GuiTest
     void readFileDoesNotThrow() throws IOException {
         new Shader("shader");
     }
