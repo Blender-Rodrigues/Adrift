d15c16432ae9726ebe79dc36bc9257afe3eae4f4
Jaak Kütt <jakutt@ttu.ee>
2020-04-09 09:17:38 +0000

    Apply suggestion to src/main/java/ee/taltech/iti0200/domain/event/handler/EntityRemoveHandler.java

--- src/main/java/ee/taltech/iti0200/domain/event/handler/EntityRemoveHandler.java
+++ src/main/java/ee/taltech/iti0200/domain/event/handler/EntityRemoveHandler.java
@@ -8,7 +8,6 @@ import ee.taltech.iti0200.domain.entity.Projectile;
 import ee.taltech.iti0200.domain.event.Event;
 import ee.taltech.iti0200.domain.event.EventBus;
 import ee.taltech.iti0200.domain.event.Subscriber;
-import ee.taltech.iti0200.domain.event.entity.DropLoot;
 import ee.taltech.iti0200.domain.event.entity.RemoveEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
