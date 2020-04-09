918496f24b78ef5e1fa62527718e4ddb113db7d5
Kristjan Kõiv <krikoi@ttu.ee>
2020-04-09 08:14:47 +0000

    Apply suggestion to src/main/java/ee/taltech/iti0200/domain/entity/HealthGlobe.java

--- src/main/java/ee/taltech/iti0200/domain/entity/HealthGlobe.java
+++ src/main/java/ee/taltech/iti0200/domain/entity/HealthGlobe.java
@@ -9,11 +9,10 @@ import java.util.Random;
 public class HealthGlobe extends Consumable implements HealingSource {
 
     private static final Vector SIZE = new Vector(1.5, 1.5);
-    private int healAmount;
+    private int healAmount = 10 + new Random().nextInt(20);
 
     public HealthGlobe(Vector position, World world) {
         super(new BoundingBox(position, SIZE), world);
-        healAmount = new Random().nextInt(20);
     }
 
     @Override
