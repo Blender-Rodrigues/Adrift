136b26425cd8cea2b47104493398c9eb49797a88
Jaak Kütt <jakutt@ttu.ee>
2020-04-09 09:17:07 +0000

    Apply suggestion to src/main/java/ee/taltech/iti0200/di/factory/ConsumableFactory.java

--- src/main/java/ee/taltech/iti0200/di/factory/ConsumableFactory.java
+++ src/main/java/ee/taltech/iti0200/di/factory/ConsumableFactory.java
@@ -1,24 +1,13 @@
 package ee.taltech.iti0200.di.factory;
 
-import com.google.inject.Inject;
-import ee.taltech.iti0200.domain.World;
 import ee.taltech.iti0200.domain.entity.Consumable;
 import ee.taltech.iti0200.domain.entity.HealthGlobe;
-import ee.taltech.iti0200.domain.entity.Living;
 import ee.taltech.iti0200.physics.Vector;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ConsumableFactory {
 
-    @Inject
-    public ConsumableFactory() {
-    }
-
     public Consumable create(Vector position) {
-        Consumable loot = new HealthGlobe(position);
-        return loot;
+        return new HealthGlobe(position);
     }
 
 }
